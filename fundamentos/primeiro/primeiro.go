47188ee6f7e8b759e4cb9bb78c8c92ef0a181f84
ma022800 <marco.vargas@intelbras.com.br>
2019-02-25 08:36:47 -0300

    included comments

--- fundamentos/primeiro/primeiro.go
+++ fundamentos/primeiro/primeiro.go
@@ -1,8 +1,13 @@
 //Pacotes executaveis iniciam pelo pacote main
 package main
 
+/*
+Os codigos Go são organizados em Pacote
+e para usá-los é necessario declarar um ou vários imports
+*/
 import "fmt"
 
+//A porta de entrada de um programa Go é a função main
 func main() {
 	fmt.Println("primeiro_teste")
 }
